e5642285dfb15ad87d1b05696ebcfcaf473375bd
Riku-KANO <rickwindows0127@gmail.com>
2024-02-12 23:12:26 +0900

    define global file

---
+++ AHC/AHC030/include/global.hpp
@@ -0,0 +1,32 @@
+#pragma once
+/**
+ * @file global.hpp
+ * @author Rick (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-12
+ * 
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include <vector>
+#include <set>
+#include <queue>
+#include <thread>
+#include <memory>
+#include <utility>
+#include <random>
+#include <chrono>
+#include <cmath>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+#include <macro.hpp>
+#include <constant.hpp>
+
+// boost multiprecision
